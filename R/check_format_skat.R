0970a3dc88318e367868411f543bc65f1335eb89
DavisBrian <Brian.Davis281@gmail.com>
2015-06-17 08:51:31 -0500

    Closes #4
    anyNA is much faster and more memory efficent for large genotype matrices.  Requires R >= 3.1.0

--- R/check_format_skat.R
+++ R/check_format_skat.R
@@ -1,5 +1,4 @@
-check_format_skat <-
-function(Z, SNPInfo, mod0, aggregateBy, snpNames){
+check_format_skat <- function(Z, SNPInfo, mod0, aggregateBy, snpNames) {
 	if(length(residuals(mod0)) != nrow(Z)) stop("Number of genotypes is not equal to number of phenotypes!")
 	
 	if(!is.null(na.action(mod0))){ 
@@ -13,5 +12,7 @@ function(Z, SNPInfo, mod0, aggregateBy, snpNames){
 	if(percent_miss < 1) warning(paste((1-percent_miss)*ncol(Z), "snps are not in SNPInfo file!"))
 	
 	if({rr <- range(Z,na.rm=TRUE); rr[1] < 0 & rr[2] > 2}) warning("Genotype possibly not dosage matrix! Alleles should be coded 0/1/2")
-	if(any(is.na(Z))) warning("Some missing genotypes - will be imputed to average dose")
+	if(anyNA(Z)) {
+    warning("Some missing genotypes - will be imputed to average dose")
+	}
 }
