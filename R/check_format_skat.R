574b643a02ff0992b1471a213100f47afa2a1af3
DavisBrian <Brian.Davis281@gmail.com>
2015-06-17 08:54:16 -0500

    Fixes #5
    Note: Changed the & to || in the bounds checking.  Now all genotypes must be [0, 2]

--- R/check_format_skat.R
+++ R/check_format_skat.R
@@ -11,7 +11,9 @@ check_format_skat <- function(Z, SNPInfo, mod0, aggregateBy, snpNames) {
 	if(percent_miss == 0) stop("Column names of Z must correspond to 'snpNames' field in SNPInfo file!")
 	if(percent_miss < 1) warning(paste((1-percent_miss)*ncol(Z), "snps are not in SNPInfo file!"))
 	
-	if({rr <- range(Z,na.rm=TRUE); rr[1] < 0 & rr[2] > 2}) warning("Genotype possibly not dosage matrix! Alleles should be coded 0/1/2")
+	if( min(Z,na.rm=TRUE) < 0 || max(Z,na.rm=TRUE) > 2) {
+    warning("Genotype possibly not dosage matrix! Alleles should be coded 0/1/2")
+	}
 	if(anyNA(Z)) {
     warning("Some missing genotypes - will be imputed to average dose")
 	}
